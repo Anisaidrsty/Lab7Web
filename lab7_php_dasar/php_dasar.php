9eb0ab31d0ee8ca7676fcc9ee4e2d42141b3224f
Anisaidrsty <101643559+Anisaidrsty@users.noreply.github.com>
2022-05-15 18:54:28 +0700

    Add files via upload

---
+++ lab7_php_dasar/php_dasar.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PHP Dasar</title>
+</head>
+<body>
+<h1>Belajar PHP Dasar</h1>
+<?php echo "Hello World";?>
+<h2>Belajar Menggunakan Variable</h2>
+<?php
+$nim = "312010040";
+$nama = 'Anisa Indriani';
+echo "NIM : " . $nim . "<br>";
+echo "Nama : $nama"; ?>
+</body>
+</html>
